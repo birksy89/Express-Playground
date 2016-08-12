b9fda34a93e04ec1adb8e22564eaf45026129a4d
Andrew <andy.birks@gmail.com>
2016-08-12 20:19:25 +0100

    Created POST function

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get('/', function (req, res) {
   res.render('index', { title: 'Hey', message: 'Hello there!'});
 });
 
+app.post('/quotes', (req, res) => {
+  console.log('Hellooooooooooooooooo!')
+})
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
