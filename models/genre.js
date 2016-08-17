b704bda94d68b4c2a39b3f62f59e40d54f4c3b9b
Andrew <andy.birks@gmail.com>
2016-08-17 21:58:53 +0100

    Created Models

---
+++ models/genre.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+//Genre Schema
+var genreSchema = mongoose.Schema({
+    name:{
+      type:String,
+      required:true
+    },
+    create_date:{
+      type: Date,
+      default: Date.now
+    }
+});
+
+var Genre = module.exports = mongoose.model('Genre', genreSchema);
